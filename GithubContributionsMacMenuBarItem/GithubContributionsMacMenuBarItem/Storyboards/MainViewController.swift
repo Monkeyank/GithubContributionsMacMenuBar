9520ec01c5c132ea9d4da2256805c1657388e0f0
Ankith Madadi <ankithm28@gmail.com>
2020-03-04 19:59:14 -0800

    Add Storyboards

---
+++ GithubContributionsMacMenuBarItem/GithubContributionsMacMenuBarItem/Storyboards/MainViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MainViewController.swift
+//  GithubContributionsMacMenuBarItem
+//
+//  Created by Ankith on 3/4/20.
+//  Copyright © 2020 Ankith. All rights reserved.
+//
+
+import SwiftUI
+
+struct MainViewController: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainViewController_Previews: PreviewProvider {
+    static var previews: some View {
+        MainViewController()
+    }
+}
---
+++ GithubContributionsMacMenuBarItem/GithubContributionsMacMenuBarItem/Storyboards/PreferencesViewController.swift
@@ -0,0 +1,21 @@
+//
+//  PreferencesViewController.swift
+//  GithubContributionsMacMenuBarItem
+//
+//  Created by Ankith on 3/4/20.
+//  Copyright © 2020 Ankith. All rights reserved.
+//
+
+import SwiftUI
+
+struct PreferencesViewController: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PreferencesViewController_Previews: PreviewProvider {
+    static var previews: some View {
+        PreferencesViewController()
+    }
+}
